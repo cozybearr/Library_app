f1ef65a6b2aea8a3906f572b66d887c323442d14
Bùi Quốc Minh Quân <buiquocminhquan.work@gmail.com>
2023-10-09 11:13:09 +0700

    Clean up homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import "./App.css";
-import { Carousel } from "./layouts/Homepage/Carousel";
-import { ExploreTopBooks } from "./layouts/Homepage/ExploreTopBooks";
-import { Heros } from "./layouts/Homepage/Hero";
-import { LibraryServices } from "./layouts/Homepage/LibraryServices";
+import { HomePage } from "./layouts/Homepage/HomePage";
+import { Carousel } from "./layouts/Homepage/components/Carousel";
+import { ExploreTopBooks } from "./layouts/Homepage/components/ExploreTopBooks";
+import { Heros } from "./layouts/Homepage/components/Hero";
+import { LibraryServices } from "./layouts/Homepage/components/LibraryServices";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 function App() {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
+      <HomePage />
       <Footer />
     </div>
   );
