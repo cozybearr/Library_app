c60ad09feba58e346b5b35419b7d10f9252f6c23
Bùi Quốc Minh Quân <buiquocminhquan.work@gmail.com>
2023-10-09 11:02:47 +0700

    Set up front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Carousel } from "./layouts/Homepage/Carousel";
+import { ExploreTopBooks } from "./layouts/Homepage/ExploreTopBooks";
+import { Heros } from "./layouts/Homepage/Hero";
+import { LibraryServices } from "./layouts/Homepage/LibraryServices";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+      <Heros />
+      <LibraryServices />
+      <Footer />
     </div>
   );
 }
